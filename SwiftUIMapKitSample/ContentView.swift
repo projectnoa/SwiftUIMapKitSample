ca6a86ef838f365e07f3ccf8f8e2eeea0c47829f
Juan <projectnoa@users.noreply.github.com>
2022-12-19 18:04:27 -0700

    Initial Commit

---
+++ SwiftUIMapKitSample/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//	ContentView.swift
+//  SwiftUIMapKitSample
+//
+//  Created by Juan Mueller on 12/19/22.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMapKitSample/SwiftUIMapKitSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//	SwiftUIMapKitSampleApp.swift
+//  SwiftUIMapKitSample
+//
+//  Created by Juan Mueller on 12/19/22.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
